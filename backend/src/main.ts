35bfe3d3e5606e42551d56ded44fb004b5d24aa4
Willi1309 <lartiguezwilians@gmail.com>
2026-03-08 13:40:28 -0400

    fix deployment 1.1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.BACKEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
